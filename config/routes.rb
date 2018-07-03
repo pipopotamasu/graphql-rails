2241e1ed6c558c7989b43e34bcce4a089fcfe2e6
pipopotamasu <ec0156hx@gmail.com>
2018-07-03 09:26:03 +0900

    add home

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   end
 
   post "/graphql", to: "graphql#execute"
+  get '/home', to: 'home#index'
+  root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
