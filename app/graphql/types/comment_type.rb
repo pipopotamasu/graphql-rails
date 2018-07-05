cef4cb875a3a86bbd1d7e9425c725354a25b06c4
pipopotamasu <ec0156hx@gmail.com>
2018-07-06 00:13:16 +0900

    get postId

--- app/graphql/types/comment_type.rb
+++ app/graphql/types/comment_type.rb
@@ -1,5 +1,5 @@
 class Types::CommentType < Types::BaseObject
   field :id, ID, null: false
-  field :post, Types::PostType, null: false
+  field :post_id, ID, null: false
   field :content, String, null: false
 end
